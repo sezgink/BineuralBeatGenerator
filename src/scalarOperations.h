29db5e9bcafb3ecd15581e2f459409bdc37ccd63
sezgink <sezginkutlu00@gmail.com>
2024-12-16 00:57:58 +0300

    Scalar operations function declarations

--- src/scalarOperations.h
+++ src/scalarOperations.h
@@ -5,3 +5,7 @@
 #include <stdint.h>
 #include <math.h>
 #define M_PI 3.14159265358979311599796346854 //PI in double precision
+
+void scale_values(double* track,double multiplier,int from,int to);
+
+void add_to_track(double* destination,double* source,int destination_from,int source_from,int length);
